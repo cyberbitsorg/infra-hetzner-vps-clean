fbf0fff70fdf5b7bc1c1fc16d93862fdd93eae61
cyberbitsorg <henk@cyberbits.org>
2026-04-18 15:38:28 +0200

    refactor: make local.fqdn a per-server map

--- locals.tf
+++ locals.tf
@@ -8,8 +8,8 @@ locals {
   cidr_ipv6_all = var.cidr_ipv6_all
   cidr_all      = [local.cidr_ipv4_all, local.cidr_ipv6_all]
 
-  # Fully Qualified Domain Name
-  fqdn = "${var.server_name}.${var.base_domain}"
+  # Fully Qualified Domain Names, keyed by server name
+  fqdn = { for k, _ in var.servers : k => "${k}.${var.base_domain}" }
 
   labels = {
     environment = var.environment
